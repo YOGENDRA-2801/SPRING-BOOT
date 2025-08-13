4f7557f9ca92abe10810aac58e6181130182c81e
YOGENDRA-2801 <yogendraydv007@gmail.com>
2025-08-13 09:25:46 +0530

    ...

---
+++ DATABASE/DATA JPA/HMS/src/main/java/com/yoy/hms/DemoClass.java
@@ -0,0 +1,13 @@
+package com.yoy.hms;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class DemoClass {
+	private final String name ;
+	private final Integer age ;
+	private final String address ;
+	
+}
